d392466ba60d521d6706795d916edeeea5ee225e
SalveOrbis <fabian.waldner@gmail.com>
2018-05-09 01:14:54 +0200

    flera filer

---
+++ bigint.h
@@ -0,0 +1,11 @@
+
+#ifndef BIGINT_H
+#define BIGINT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "bigintmacros.h"
+#include "bigintimplement.h"
+#include "biginthelp.h"
+
+#endif /* BIGINT_H*/
